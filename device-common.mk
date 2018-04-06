8457036a335d14775c442eb45986e728f948bad6
Svyatoslav Ryhel <clamor95@gmail.com>
2018-04-06 23:51:43 +0300

    transformer: remove NFC
    Change-Id: I8d738d6e04be07cc3222c96730d87b6aaf7728d5
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -36,7 +36,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
@@ -45,10 +44,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
-
-# File that declares the MIFARE NFC constant
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
 
 # Input device configs
 PRODUCT_COPY_FILES += \
@@ -93,13 +88,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     device/asus/grouper/audio/audio_policy.conf:system/etc/audio_policy.conf \
     device/asus/grouper/audio/mixer_paths.xml:system/etc/mixer_paths.xml
-
-# NFC
-PRODUCT_PACKAGES += \
-    nfc.grouper \
-    libpn544_fw \
-    NfcNxp \
-    Tag
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
