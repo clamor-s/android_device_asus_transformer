3d22635bcd334159ca44cbe648edb2b19a26a884
Svyatoslav Ryhel <clamor95@gmail.com>
2018-10-31 08:57:47 +0200

    Binderize lights HAL
    Change-Id: Id35a33cef346a81048312d74d8df2f1c551113b8
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>
    Reviewed-on: https://gerrit.unlegacy-android.org/10679

--- device-common.mk
+++ device-common.mk
@@ -86,6 +86,8 @@ PRODUCT_PACKAGES += \
 
 # Lights HAL
 PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
     lights.transformer
 
 # Sensors HAL
