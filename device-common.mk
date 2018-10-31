7f92b00bf06c8c876718bd55755be3fa6c0339c8
Svyatoslav Ryhel <clamor95@gmail.com>
2018-10-31 21:10:45 +0200

    Binderize sensors HAL
    Change-Id: I3ae3186a490044ee0aec44647de59d9fb37f7a5c
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>
    Reviewed-on: https://gerrit.unlegacy-android.org/10685

--- device-common.mk
+++ device-common.mk
@@ -92,6 +92,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors HAL
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
     sensors.transformer \
     sensors.tegra \
     libmllite \
