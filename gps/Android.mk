e3457fa3847ba3d8c135f9b9927a16b9f4d26d1f
Svyatoslav Ryhel <clamor95@gmail.com>
2018-11-08 17:03:14 +0200

    gps: re-organize shared libraries inclusions
    Our GPS HAL require legacy libstlport which isn't build from source
    anymore. Include prebuilt one and place it with libgpsd-compat.so into
    /vendor/lib.
    
    Change-Id: Ie53b39841801abc2fcf6dd3b480315c2a9eb0974
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,5 +1,5 @@
+#
 # Copyright (C) 2015 The Android Open Source Project
-# Written by Dmitry Grinberg
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,14 +12,25 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_SHARED_LIBRARIES := liblog libhardware_legacy libgui libbinder libutils libsensor
-LOCAL_SRC_FILES := libgpsd-compat.c
 LOCAL_MODULE := libgpsd-compat
+LOCAL_PROPRIETARY_MODULE := true
 LOCAL_MODULE_TAGS := optional
-
+LOCAL_SRC_FILES := /libgpsd-compat/libgpsd-compat.c
+LOCAL_SHARED_LIBRARIES := liblog libhardware_legacy libgui \
+                          libbinder libutils libsensor
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libstlport
+LOCAL_SRC_FILES := /prebuilt/libstlport.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_PROPRIETARY_MODULE := true
+OVERRIDE_BUILT_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+include $(BUILD_PREBUILT)
