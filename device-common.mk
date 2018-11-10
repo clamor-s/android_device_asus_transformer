8fcd9790085a6c31b2d43283c46b62ced679976b
Svyatoslav Ryhel <clamor95@gmail.com>
2018-11-10 16:31:07 +0200

    Binderize vibrator HAL
    Change-Id: Ic411df32d2fec295fe1a6c676129e709a1f7ab70
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>
    Reviewed-on: https://gerrit.unlegacy-android.org/10703

--- device-common.mk
+++ device-common.mk
@@ -89,6 +89,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
 # Lights HAL
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
